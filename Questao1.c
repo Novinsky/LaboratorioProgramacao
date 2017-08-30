eee8184971d2cc6b2131f529e525e34d7925435b
Renan Bona <renan3094@gmail.com>
2017-08-29 21:15:52 -0300

    Funcao nota final criada com sucesso

--- Questao1.c
+++ Questao1.c
@@ -24,12 +24,23 @@ float nota_valida(float nota1, float nota2, float nota3)
   return nota_selecionada;
 }
 
+void nota_final(float *originalidade,float *beleza){
+  float final;
+  final = (*originalidade * 0.6) + (*beleza * 0.4);
+  printf("%.2f\n", final);
+}
 
 int main() {
 
-  int i, id_inscricao;
-  float notas[3], nota_teste, x;
+  int i, id_inscricao, repete = 2;
+  float notas[3], nota_teste, x, nota_originalidade, nota_beleza;
+  float *originalidade, *beleza;
 
+  while(repete > 0){
+  if (repete == 2)
+    printf("NOTAS DO CRITERIO ORIGINALIDADE!!!\n");
+  if (repete == 1)
+    printf("NOTAS DO CRITERIO BELEZA!!!\n");
   for(i = 0; i < 3; i++){
     do{
     printf("Digite a nota do jurado %d: ", i);
@@ -41,8 +52,19 @@ int main() {
         printf("A nota digitada foi invalida, ela deve ser maior ou igual a 0!!!\n");
     }while(nota_teste < 0);
   }
-  x = nota_valida(notas[0], notas[1], notas[2]);
-  printf("%.2f", x);
+    if(repete == 2){
+      nota_originalidade = nota_valida(notas[0], notas[1], notas[2]);
+    }
+    if(repete == 1){
+      nota_beleza = nota_valida(notas[0], notas[1], notas[2]);
+    }
+    repete--;
+  }
+  originalidade = &nota_originalidade;
+  beleza = &nota_beleza;
+  printf("A nota final do candidato foi: ");
+  nota_final(originalidade, beleza);
+
 
   return 0;
 }
